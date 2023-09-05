930f221859487339678100e0e96f5d85cbb31b05
lucaso-silva <luc.osilva@gmail.com>
2023-09-04 22:59:12 -0700

    build: access data json

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,17 @@ const selectInput = document.querySelector("select");
 const buttonClear = document.querySelector("button");
 const filterItensArea = document.querySelector(".filter-options");
 
+
+
+
+fetch("data.json").then((response)=>{
+    response.json().then((dados)=>{
+        dados.map((dado) => {
+            console.log(`${dado.company}, ${dado.position}`)
+        });
+    })
+})
+
 selectInput.addEventListener("change", createFilterItem);
 buttonClear.addEventListener("click", ()=>{
     filterItensArea.textContent = "";
