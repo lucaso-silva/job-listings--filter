b10690e24b18080416c9b896756256bb1d8901bb
lucaso-silva <luc.osilva@gmail.com>
2023-09-05 22:37:45 -0700

    feat(wip) function create card - error objectHTMLDivElement

--- assets/js/createCard.js
+++ assets/js/createCard.js
@@ -1,17 +1,92 @@
+const cardsContainer = document.querySelector(".cards-container");
 
 export default function createCard(item){
-    const cardsContainer = document.querySelector(".cards-container");
+    let companyLogo = item.logo;
     let companyName = item.company;
     let position = item.position;
     let postedAt = item.postedAt;
     let typeContract = item.contract;
     let jobLocation = item.location;
     let languages = item.languages;
+    let level = item.level;
     let tools = item.tools;
 
     console.log(`${companyName} - ${position} / posted ${postedAt}. 
     Location: ${jobLocation} - ${typeContract}. 
     Languages needed: ${languages}. Assets: ${tools}`)
 
-    cardsContainer.innerHTML += companyName
+    let newCard = document.createElement("div");
+    newCard.classList.add("card");
+
+    let companyLogoImg = document.createElement("img");
+    companyLogoImg.setAttribute("src", companyLogo);
+    companyLogoImg.setAttribute("alt", "Company logo");
+
+    let divGeneralInfo = document.createElement("div");
+    divGeneralInfo.classList.add("general-info");
+
+    newCard.appendChild(companyLogoImg);
+    newCard.appendChild(divGeneralInfo);
+
+    let divCompanyRole = document.createElement("div");
+    divCompanyRole.classList.add("company-role");
+
+    divGeneralInfo.appendChild(divCompanyRole)
+
+    let headingCompanyName = document.createElement("h1");
+    headingCompanyName.textContent = companyName;
+    
+    let infoNew = document.createElement("span");
+    infoNew.classList.add("new");
+    // infoNew.textContent = 
+
+    let infoFeatured = document.createElement("span");
+    infoFeatured.classList.add("featured");
+    // infoFeatured.textContent = 
+
+    let headingPosition = document.createElement("h2");
+    headingPosition.textContent = position;
+
+    divCompanyRole.appendChild(headingCompanyName);
+    divCompanyRole.appendChild(infoNew);
+    divCompanyRole.appendChild(infoFeatured);
+    divCompanyRole.appendChild(headingPosition);
+
+    let divInfo = document.createElement("div");
+    divInfo.classList.add("info");
+
+    divGeneralInfo.appendChild(divInfo);
+
+    let infoDayPosted = document.createElement("p");
+    infoDayPosted.textContent = postedAt;
+
+    let infoTypeContract = document.createElement("p");
+    infoTypeContract.textContent = typeContract;
+
+    let infoLocation = document.createElement("p");
+    infoLocation.innerText = jobLocation;
+
+    divInfo.appendChild(infoDayPosted);
+    divInfo.appendChild(infoTypeContract);
+    divInfo.appendChild(infoLocation);
+
+    let divMoreInfo = document.createElement("div");
+    divMoreInfo.classList.add("more-info");
+
+    newCard.appendChild(divMoreInfo);
+
+    let infoLanguage = document.createElement("p");
+    infoLanguage.innerText = languages;
+
+    let infoLevel = document.createElement("p");
+    infoLevel.innerText = level;
+
+    let infoTools = document.createElement("p");
+    infoTools.innerText = tools;
+
+    divMoreInfo.appendChild(infoLanguage);
+    divMoreInfo.appendChild(infoLevel);
+    divMoreInfo.appendChild(infoTools);
+
+    cardsContainer.innerHTML += newCard;
 }
