1a6ce576bbacccfa73dee6f933f0d57774921da9
lucaso-silva <luc.osilva@gmail.com>
2023-09-15 23:31:59 -0700

    unsucced function filter

--- assets/js/filterJobs.js
+++ assets/js/filterJobs.js
@@ -1,16 +1,32 @@
-// let listOptions = [];
-
+let newCard = {
+  role: "",
+  level: ""
+};
 
 export default function filterJobs() {
   let filtersSelected = document.querySelectorAll(".option");
+  let groupRole = document.querySelector(".role");
+  let groupLevel = document.querySelector(".level");
+  let groupLanguage = document.querySelectorAll(".language");
+  let groupTools = document.querySelectorAll(".tools");
+
   console.log(filtersSelected);
+  console.log("role: " + groupRole.textContent.toLowerCase());
+  console.log("level: " + groupLevel.textContent.toLowerCase());
+  
+  groupLanguage.forEach((lang)=>{
+    console.log("languages: " + lang.textContent.toLowerCase());
+  });
 
-  // let newCard = {
-  //   role: groupRole,
-  //   level: groupLevel,
-  //   laguage: groupLanguage,
-  //   tools: groupTools
-  // };
+  groupTools.forEach((tool)=>{
+    console.log("tools: " + tool.textContent.toLowerCase());
+  });
+  // console.log("languages: " + groupLanguage);
+  // console.log("tools: " + groupTools);
 
-  // console.log("newCard: " + newCard)
+  newCard.role = groupRole.textContent.toLowerCase();
+  newCard.level = groupLevel.textContent.toLowerCase();  
+
+  console.log("newCard: " + newCard.role)
+  console.log(typeof newCard)
 }
